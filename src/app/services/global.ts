fd7a46f3282d8b5d5d80a0b31ff3acc0ce97c8c2
Marianoantar <marianoantar@gmail.com>
2024-10-22 14:58:57 -0300

    Cambios en la entrada de proyectos para hacer la ventana de texto mas grande.

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export const Global = {
     // url backend
-    // url: 'http://localhost:3000/api/'
+    // url: 'http://localhost:3000/api/',
     url: 'https://portfolio.storum.com.ar/api/',
 
     // Codigo de edicion 
